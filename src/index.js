5b25f7fa1ea2ff0592cc99941ff6b09c6c066726
pudu87 <sam.cappelle87@gmail.com>
2020-12-18 14:27:49 -0300

    add processData function

--- src/index.js
+++ src/index.js
@@ -6,11 +6,25 @@ async function fetchData(search) {
   try {
     console.log(urlStart + search + urlEnd)
     const response = await fetch(urlStart + search + urlEnd, {mode: 'cors'});
-    const data = await response.json();
-    console.log(data);
+    const dataSet = await response.json();
+    console.log(dataSet);
+    console.log(processData(dataSet))
   } catch(error) {
     console.log(error);
   }
+}
+
+function processData(data) {
+  const day = data.sys.sunrise < data.dt && data.dt < data.sys.sunset;
+  const requiredData = { 
+    weather: data.weather[0].main,
+    temp: data.main.temp, 
+    pressure: data.main.pressure, 
+    humidity: data.main.humidity, 
+    wind: data.wind.deg,
+    day
+  }
+  return requiredData
 }
 
 fetchData(search);
