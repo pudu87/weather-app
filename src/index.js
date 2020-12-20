dc6465e4db164b62a1eaed683e2a0aacac3a2028
pudu87 <sam.cappelle87@gmail.com>
2020-12-20 19:21:09 -0300

    add tempSwitch for fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const search = document.querySelector('#location');
 const form = document.querySelector('form');
 const city = document.querySelector('.city');
-const temp = document.querySelector('.temp span');
+const tempQuantity = document.querySelector('.temp-quantity');
+const tempUnit = document.querySelector('.temp-unit');
+const tempSwitch = document.querySelector('.temp-switch');
 const windDir = document.querySelector('.arrow');
 const windSpeed = document.querySelector('.speed')
 const weather = document.querySelector('.weather img');
@@ -10,9 +12,11 @@ const humidity = document.querySelector('.humidity span');
 const urlStart = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const urlEnd = '&appid=30aa43e809e468d20a5ff0a5e98a95ab';
 
+const data = {};
+
 function displayLocation(e) {
   e.preventDefault();
-  const location = search.value.replace(/\s+/g, '');
+  const location = search.value.replace(/\s+/g, '+');
   fetchData(location);
 }
 
@@ -21,33 +25,28 @@ async function fetchData(search) {
     const response = await fetch(urlStart + search + urlEnd, {mode: 'cors'});
     const dataSet = await response.json();
     const data = processData(dataSet);
-    displayData(data);
-    console.log(dataSet);
-    console.log(processData(dataSet));
+    displayData();
   } catch(error) {
     console.log(error);
   }
 }
 
-function processData(data) {
-  const day = data.sys.sunrise < data.dt && data.dt < data.sys.sunset;
-  const requiredData = {
-    name: data.name, 
-    temp: data.main.temp, 
-    windDir: data.wind.deg,
-    windSpeed: data.wind.speed,
-    icon: data.weather[0].icon,
-    pressure: data.main.pressure, 
-    humidity: data.main.humidity, 
-    day
-  }
-  return requiredData
+function processData(dataSet) {
+  const day = dataSet.sys.sunrise < dataSet.dt && dataSet.dt < dataSet.sys.sunset;
+  data.name = dataSet.name;
+  data.temp = dataSet.main.temp; 
+  data.windDir = dataSet.wind.deg;
+  data.windSpeed = dataSet.wind.speed;
+  data.icon = dataSet.weather[0].icon;
+  data.pressure = dataSet.main.pressure; 
+  data.humidity = dataSet.main.humidity; 
+  data.day = day;
 }
 
-function displayData(data) {
+function displayData() {
   city.textContent = data.name;
-  const tempC = Math.round(data.temp - 273.15);
-  temp.textContent = `${tempC} °C`;
+  switchTemp(data.temp);
+  switchTemp(data.temp);
   windDir.style.rotate = `${data.windDir - 180}deg`;
   windSpeed.textContent = `${data.windSpeed} m/s`
   weather.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
@@ -55,5 +54,16 @@ function displayData(data) {
   humidity.textContent = `${data.humidity} %`;
 }
 
+function switchTemp() {
+  if (tempUnit.textContent === '°C') {
+    tempQuantity.textContent = Math.round((data.temp - 273.15) * 9 / 5 + 32);
+    tempUnit.textContent = '°K';
+  } else {
+    tempQuantity.textContent = Math.round(data.temp - 273.15);
+    tempUnit.textContent = '°C';
+  }
+}
+
 fetchData('Santiago');
 form.addEventListener('submit', displayLocation);
+tempSwitch.addEventListener('click', switchTemp);
